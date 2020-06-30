43467293601f21af5426155c51d66727e759453c
winifredf <winifred.dykes@hotmail.com>
2020-06-30 09:15:18 -0400

    created seed file in database

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+6.times do
+    User.create()
+end
+
+
+# <User id: nil, name: nil, age: nil, email: nil, created_at: nil, updated_at: nil>
+
+# <Skill id: nil, title: nil, body: nil, user_id: nil, created_at: nil, updated_at: nil>
