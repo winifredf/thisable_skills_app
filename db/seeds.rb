29037aa44a0221b3d6116eee509f4699afded4cf
winifredf <winifred.dykes@hotmail.com>
2020-06-30 09:27:11 -0400

    created seed data for user name, age, email via faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 6.times do
-    User.create()
+    User.create(name: Faker::Name.name, age: Faker::Number.between(from: 18, to: 52), email: Faker::Internet.email)
+    Skill.create(title: , body: , user_id:1)
+    Skill.create(title: , body: , user_id:2)
+    Skill.create(title: , body: , user_id:3)
+    Skill.create(title: , body: , user_id:4)
+    Skill.create(title: , body: , user_id:5)
+    Skill.create(title: , body: , user_id:6)
 end
+
 
 
 # <User id: nil, name: nil, age: nil, email: nil, created_at: nil, updated_at: nil>
